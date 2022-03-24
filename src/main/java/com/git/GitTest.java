ddbacd2819caab76fb9f56e75191f537998eaae1
Lijie <2085576513@qq.com>
2022-03-24 16:24:40 +0800

    third commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,6 +3,7 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git1");
-        System.out.println("hello,git1");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
     }
 }
