9daaaaaeef5e3e7846c07ad19d91f611f3018298
Lijie <2085576513@qq.com>
2022-03-24 17:06:23 +0800

    fifth commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("hello,git1");
         System.out.println("hello,hot-fix");
         System.out.println("hello,hot-fix 2");
+        System.out.println("萨瓦迪卡");
+
     }
 }
